86715501721f01068389b2d559ce5491da9e391a
Zhlobochka <Varyazhloba@yandex.ru>
2022-12-11 13:53:27 +0400

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/**/*.{js,ts,vue,jsx}"
+  ],
+  theme: {
+    colors: {
+      'white': '#fff',
+
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
